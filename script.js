1d52444286fbca39b35c2b714c179d782474d50f
tonyacurrans <126038586+tonyacurrans@users.noreply.github.com>
2023-03-02 18:40:33 -0800

    added script

---
+++ script.js
@@ -0,0 +1,7 @@
+AOS.init();
+      var typed = new Typed(".animate", {
+        strings: ["A New Alien Colony", "Pods", "Dwellings", "Hives"],
+        typeSpeed: 80,
+        backSpeed: 50,
+        loop: true,
+      });
